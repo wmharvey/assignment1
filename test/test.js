33f9a9a6bfb0da9e6ce329d073795f3e4121aea3
Whitney Harvey <wmelrose01@gmail.com>
2016-01-04 21:17:25 -0800

    Rewrite tests to trim whitespace

--- test/test.js
+++ test/test.js
@@ -4,16 +4,15 @@ var spawn = require('child_process').spawn;
 
 describe('greet', function() {
   it('should return a string with hello + name', function() {
-    var g = greet('Whitney');
-    assert.deepEqual(g, 'hello Whitney', 'the output string is not as expected');
+    var g = greet('Red');
+    assert.equal(g, 'hello Red', 'the output string is not as expected');
   })
 
-  it('should print a string from a CL argument', function(done) {
-    var g = spawn('node', ['./greet.js', 'Whitney']);
-    g.stdout.on('data', function(data) {
-      console.log('stout: ' + data);
-      assert( /hello Whitney/.test(data.toString() ), 'the output did not contain the expected string');
+  it('should print a string from a CL command', function(done) {
+    var command = spawn('node', ['./greet.js', 'Blue']);
+    command.stdout.on('data', function(data) {
+      assert.equal( data.toString().trim(), 'hello Blue', 'the output did not contain the expected string');
       done();
-    })
+    });
   })
 })
